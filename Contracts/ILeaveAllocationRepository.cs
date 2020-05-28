34661af016479e6816ada1d9a6269803221bbb01
Jerry Peeler <jerry11peeler@gmail.com>
2020-05-28 16:07:02 -0400

    Added leave Type repositories and added datatables ( font awesome icons + crud

---
+++ Contracts/ILeaveAllocationRepository.cs
@@ -0,0 +1,14 @@
+﻿using Leave_Managment.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Leave_Managment.Contracts
+{
+    public interface ILeaveAllocationRepository : IRepositoryBase<LeaveAllocation>
+    {    
+
+      
+    }
+}
---
+++ Repository/LeaveHistoryRepository.cs
@@ -0,0 +1,56 @@
+﻿using Leave_Managment.Contracts;
+using Leave_Managment.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Leave_Managment.Repository
+{
+    public class LeaveHistoryRepository : ILeaveHistoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public LeaveHistoryRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool Create(LeaveHistory entity)
+        {
+            _db.LeaveHistories.Add(entity);
+            return Save();            
+        }
+
+        public bool Delete(LeaveHistory entity)
+        {
+            _db.LeaveHistories.Remove(entity);
+            return Save();
+        }
+
+        public ICollection<LeaveHistory> FindAll()
+        {
+            var LeaveHistories = _db.LeaveHistories.ToList();
+            return LeaveHistories;
+        }
+
+        public LeaveHistory FindById(int id)
+        {
+            var leaveHistory = _db.LeaveHistories.Find(id);
+            //or alternately   _db.LeaveHistories.FirstOrDefault();
+            return leaveHistory;
+        }
+       
+        public bool Save()
+        {
+            var changes = _db.SaveChanges();
+            return changes > 0;
+
+            // or alternatively  return _db.SaveChanges() > 0;
+        }
+
+        public bool Update(LeaveHistory entity)
+        {
+            _db.LeaveHistories.Update(entity);
+            return Save();
+        }
+    }
+}
